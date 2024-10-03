373df4c17fb20d5a9566cd471f08a31d56c096be
Abdullah ridha <37846925+lrq33@users.noreply.github.com>
2024-10-03 15:24:13 +0300

    Add project files.

---
+++ keylogger/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace testpointer
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            string processName = "chrome";
+
+            while (true)
+            {
+                await PrintTabTitlesAsync(processName);
+                await Task.Delay(9000); 
+            }
+        }
+
+        private static async Task PrintTabTitlesAsync(string processName)
+        {
+            Process[] chromeProcesses = Process.GetProcessesByName(processName);
+
+            foreach (Process process in chromeProcesses)
+            {
+                IntPtr hwnd = process.MainWindowHandle;
+                string title = await GetWindowTitleAsync(hwnd);
+                Console.WriteLine($"Tab Title ({process.Id}): {title}");
+            }
+        }
+
+        private static Task<string> GetWindowTitleAsync(IntPtr hWnd)
+        {
+            return Task.Run(() =>
+            {
+                StringBuilder title = new StringBuilder(256);
+                GetWindowText(hWnd, title, title.Capacity);
+                return title.ToString();
+            });
+        }
+
+        [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder lpString, int nMaxCount);
+    }
+}
